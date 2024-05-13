366b6a5d7004a4649d1e39ed033d71bad288e65f
pachorsc <jfrodriano@gmail.com>
2024-05-13 11:34:47 +0200

    Subo el proyecto primera parte

---
+++ PracticaFinal-pt.1/FinalProject/src/finalproject/FinalProject.java
@@ -0,0 +1,17 @@
+
+package finalproject;
+
+import Menus.*;
+
+
+public class FinalProject {
+
+
+    public static void main(String[] args) {
+        
+        Eleccion menu_prin = new Eleccion();
+        menu_prin.setVisible(true);
+        menu_prin.setLocationRelativeTo(null);
+    }
+    
+}
