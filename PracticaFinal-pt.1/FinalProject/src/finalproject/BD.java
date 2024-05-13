a17c05d9c4c10543a61920ddd3cc597879274eff
daniolmedo855 <167980742+daniolmedo855@users.noreply.github.com>
2024-05-13 12:25:48 +0200

    Insert

---
+++ PracticaFinal-pt.1/FinalProject/src/finalproject/BD.java
@@ -0,0 +1,34 @@
+package bd;
+
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+
+public class BD {
+    public void basedatos() throws ClassNotFoundException, SQLException {
+        Scanner sc = new Scanner(System.in, "ISO-8859-2");
+        String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+        String username = "aula";
+        String pass = "aula";
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+        
+        System.out.println("Introduce el nombre y contraseña");
+        String n=sc.nextLine();
+        String c=sc.nextLine();
+        
+        String consulta = insertarUsuarios(n, c);
+         
+        System.out.println("Conexion establecida");
+        ResultSet rs = st.executeQuery(consulta);
+        
+        con.close();
+    }
+    
+    public String insertarUsuarios(String nombre, String contrasenia){
+        String s="";
+        s="insert into usuarios values ('"+ nombre +"', "+ contrasenia +", )";
+        return s;
+    }
+}
