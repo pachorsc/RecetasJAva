9e54379ce077fe0bab1eb20f6810edfe044e65df
daniolmedo855 <167980742+daniolmedo855@users.noreply.github.com>
2024-05-14 11:50:29 +0200

    Base de datos etiquetas y receta_etiqueta

--- PracticaFinal-pt.1/FinalProject/BD_Practica.sql
+++ PracticaFinal-pt.1/FinalProject/BD_Practica.sql
@@ -1,5 +1,7 @@
-drop table usuarios;
 drop table recetas;
+drop table usuarios;
+drop table etiquetas;
+drop table receta_etiqueta;
 
 CREATE TABLE USUARIOS (
     nombre varchar2(20) primary key,
@@ -13,8 +15,29 @@ CREATE TABLE RECETAS (
     autor varchar2(20),
     ingredientes varchar2(50),
     pasos varchar2(50),
-    puntuacion varchar2(20) check (puntuacion between 0 and 10),
-    constraint fk_re_us foreign key (creador) references usuarios (nombre) 
+    constraint fk_re_us foreign key (autor) references usuarios (nombre) 
 );
+
+CREATE TABLE ETIQUETAS(
+    cod integer GENERATED ALWAYS AS IDENTITY (Start with 1 increment by 1) primary key,
+    nombre varchar2(20) not null
+);
+
+CREATE TABLE RECETA_ETIQUETA(
+    receta integer,
+    etiqueta number(2,0),
+    CONSTRAINT pk_re_et primary key(receta, etiqueta)
+);
+
+insert into USUARIOS values ('ADMIN', ADMIN, 1);
+
+insert into ETIQUETAS (nombre) values ('Desayuno');
+insert into ETIQUETAS (nombre) values ('Almuerzo');
+insert into ETIQUETAS (nombre) values ('Cena');
+insert into ETIQUETAS (nombre) values ('Dulce');
+insert into ETIQUETAS (nombre) values ('Salado');
+insert into ETIQUETAS (nombre) values ('Carne');
+insert into ETIQUETAS (nombre) values ('Vegetariano');
+insert into ETIQUETAS (nombre) values ('Sano');
 
 commit;
