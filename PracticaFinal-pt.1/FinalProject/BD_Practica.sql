2c215c8a92194b0b98d0cf61b76041fa0271c02e
daniolmedo855 <167980742+daniolmedo855@users.noreply.github.com>
2024-05-14 11:22:31 +0200

    Base de datos codigo incremeta

--- PracticaFinal-pt.1/FinalProject/BD_Practica.sql
+++ PracticaFinal-pt.1/FinalProject/BD_Practica.sql
@@ -8,13 +8,13 @@ CREATE TABLE USUARIOS (
 );
 
 CREATE TABLE RECETAS (
-    cod number(2,0) primary key,
+    cod integer GENERATED ALWAYS AS IDENTITY (Start with 1 increment by 1) primary key,
     nombre varchar2(20) not null,
     autor varchar2(20),
     ingredientes varchar2(50),
     pasos varchar2(50),
     puntuacion varchar2(20) check (puntuacion between 0 and 10),
-    constraint fk_re_us foreign key (creador) references usuarios (nombre)
+    constraint fk_re_us foreign key (creador) references usuarios (nombre) 
 );
 
 commit;
