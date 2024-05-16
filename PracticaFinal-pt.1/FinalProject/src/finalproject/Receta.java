150d5e4a33867331874b5e4cf9c1cfb677478550
EAG <EAG@A5PC14>
2024-05-16 11:48:14 +0200

    Cambio en Receta

--- PracticaFinal-pt.1/FinalProject/src/finalproject/Receta.java
+++ PracticaFinal-pt.1/FinalProject/src/finalproject/Receta.java
@@ -3,17 +3,31 @@ package finalproject;
 
 
 public class Receta {
-    String nombre;
-    String autor;
-    String pasos;
+    private int cod;
+    private String nombre;
+    private String autor;
+    private String ingredientes;
+    private String pasos;
     
     public Receta(){
-        
+        nombre = "";
+        autor = "";
+        ingredientes = "";
+        pasos = "";
     }
+    
     public Receta(String nom, String aut, String pas){
         this.nombre = nom;
         this.autor = aut;
         this.pasos = pas;
+    }
+
+    public Receta(int cod, String nombre, String autor, String ingredientes, String pasos) {
+        this.cod = cod;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.ingredientes = ingredientes;
+        this.pasos = pasos;
     }
 
     public String getNombre() {
@@ -39,6 +53,13 @@ public class Receta {
     public void setPasos(String pasos) {
         this.pasos = pasos;
     }
-    
-    
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+      
 }
