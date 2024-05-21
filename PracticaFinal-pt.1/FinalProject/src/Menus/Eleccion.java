9564458da9d789b32713e0da9007102f4731f2ed
unknown <Daniolmedo855@gmail.com>
2024-05-21 12:03:03 +0200

    Puntuaciones

--- PracticaFinal-pt.1/FinalProject/src/Menus/Eleccion.java
+++ PracticaFinal-pt.1/FinalProject/src/Menus/Eleccion.java
@@ -35,14 +35,18 @@ public class Eleccion extends javax.swing.JFrame {
         
         ResultSet receta = BD.datos(BD.select_receta());
         receta.next();
+        ResultSet puntuaciones;
         ResultSet usu;
         
         do{//Se hace el llenado de la tabla con los datos que se obtienen  de la consulta
+            datos[2]="";
             usu = BD.datos(BD.select_usu()+BD.select_condicion("cod", receta.getString(3)));
+            puntuaciones= BD.datos(BD.select("count(*), sum(nota)","puntuaciones")+BD.select_condicion("receta", receta.getString(3)));
+            puntuaciones.next();
             usu.next();
             datos[0] = receta.getString(2);
             datos[1] = usu.getString(2);
-            datos[2] = receta.getString(5);
+            datos[2] += puntuaciones.getFloat(2)/ puntuaciones.getFloat(1);
             model.addRow(datos);
         }while(receta.next());
        
